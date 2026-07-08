b99641ce4d9c845ab4fbda9cc7adb2550effc4a5
FitFocusHub <FitFocusHub@users.noreply.github.com>
2026-07-08 07:11:08 +0000

    deploy: af453074efd5674782ba7b7fbc6edde029d315be

--- assets/Contact-CR06squd.js
+++ assets/Contact-CR06squd.js
@@ -1,4 +1,4 @@
-import{j as e,c as i,b,S as t,T as v,U as m,V as f,W as d,X as N,h as F,Y as w}from"./index-ByXBoB8G.js";import{r as h}from"./vendor-BugZiGSO.js";import{s as S}from"./services-Mnq_cU5t.js";import{A as u}from"./AnimatedSection-CUQDtlQn.js";import{P as y}from"./PageHeader-BmtfQbDi.js";import"./icons-C95SPkpI.js";import"./motion-Ds_HE4Dz.js";function q(){const[a,r]=h.useState({name:"",phone:"",email:"",business:"",service:"",budget:"",message:""}),[o,c]=h.useState(!1),l=s=>{r({...a,[s.target.name]:s.target.value})},p=s=>{s.preventDefault(),c(!0);const n=`
+import{j as e,c as i,b,S as t,T as v,U as m,V as f,W as d,X as N,h as F,Y as w}from"./index-CFDph2Op.js";import{r as h}from"./vendor-BugZiGSO.js";import{s as S}from"./services-Mnq_cU5t.js";import{A as u}from"./AnimatedSection-CLxZ7fnH.js";import{P as y}from"./PageHeader-BhjAlRCM.js";import"./icons-C95SPkpI.js";import"./motion-Ds_HE4Dz.js";function q(){const[a,r]=h.useState({name:"",phone:"",email:"",business:"",service:"",budget:"",message:""}),[o,c]=h.useState(!1),l=s=>{r({...a,[s.target.name]:s.target.value})},p=s=>{s.preventDefault(),c(!0);const n=`
 *New Project Inquiry*
 ━━━━━━━━━━━━━━━━━━
 👤 *Name:* ${a.name}
